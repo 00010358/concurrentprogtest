145d5603d8b6be81b1c718abe19cb8dd5da70a2d
00010358 <73095425+00010358@users.noreply.github.com>
2023-03-07 13:54:59 +0500

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(25, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(283, 276);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(400, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(283, 276);
+            this.listBox2.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(233, 314);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(705, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(413, 276);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1153, 377);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CP_MockTest_DLL;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        FileServer fs = FileServer.GetInstance();
+
+
+        object obj = new object();
+        SemaphoreSlim sl = new SemaphoreSlim(4, 4);
+        ReaderWriterLock rw = new ReaderWriterLock();
+        CountdownEvent cde = new CountdownEvent(10);
+        string path = "txt.txt";
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            init();
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+
+        void init() {
+
+            btnStart.Enabled = false;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread t = new Thread(GetAndSave);
+                t.Start(i);
+            }
+
+            new Thread(
+                () => {
+
+                    cde.Wait();
+
+                    invoke(() =>
+                    {
+                        btnStart.Enabled = true;
+                        readText();
+                    });
+
+                    cde.Reset();
+                }
+            ).Start();
+
+            
+        }
+
+        void GetAndSave(object threadId) {
+
+            sl.Wait();
+
+            int id = (int) threadId;
+
+            // thread start info
+
+            invoke(() =>
+            {
+                listBox1.Items.Add(string.Format($"Thread {id} starting"));
+            });
+
+            // get msg
+
+            byte[] array = fs.GetFile(id);
+
+            rw.AcquireWriterLock(Timeout.Infinite);
+            AppendAllBytes(path, array);
+
+            //File.WriteAllBytes(@"D:\WIUT Classes\lvl6\ConcurrentProg\Mocks\bis2_3\txt\nex.txt", array);
+
+            rw.ReleaseWriterLock();
+
+
+
+            string msg = Encoding.Default.GetString(array);
+
+            // insert msg
+
+            invoke(() =>
+            {
+                listBox2.Items.Add(msg);
+            });
+
+            // thread finish info
+
+            invoke(() =>
+            {
+                listBox1.Items.Add(string.Format($"Thread {id} finished"));
+            });
+
+            cde.Signal();
+
+            sl.Release();
+            
+
+
+        }
+        public static void AppendAllBytes(string path, byte[] bytes)
+        {
+
+
+            using (var stream = new FileStream(path, FileMode.Append))
+            {
+                stream.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        void invoke(Action func)
+        {
+            Invoke(
+                new MethodInvoker(
+                    () => {
+                        func();
+                    }
+                )
+            );
+        }
+
+
+        void readText()
+        {
+            rw.AcquireReaderLock(Timeout.Infinite);
+            string txt = File.ReadAllText(path);
+
+            rw.ReleaseReaderLock();
+
+            textBox1.Text = txt;
+        }
+    }
+}
